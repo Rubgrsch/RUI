b6a2a3030e71e5c330120d6ee939ae6d7a267804
Rubgrsch <rubgrsch@gmail.com>
2020-11-19 23:24:20 +0800

    Chat copy

--- Modules/Chat/Chat.lua
+++ Modules/Chat/Chat.lua
@@ -1,6 +1,54 @@
 local _, rui = ...
 local B, L, C = unpack(rui)
 
+-- chat copy
+local function CopyChat()
+	local frame, t, idx = ChatFrame1, {}, 1
+	for i = 1, frame:GetNumMessages() do
+		local msg, r, g, b = frame:GetMessageInfo(i)
+		if not msg:find("|K") then -- not need to copy protected string
+			r, g, b = r or 1, g or 1, b or 1
+			t[idx] = format("|cff%02x%02x%02x%s|r",r*255,g*255,b*255,msg)
+			idx = idx + 1
+		end
+	end
+	return table.concat(t, "\n")
+end
+
+local copy = CreateFrame("Button")
+copy:SetPoint("LEFT", ChatFrameChannelButton, "RIGHT")
+copy:SetSize(20,20)
+local texture = copy:CreateTexture(nil, "ARTWORK")
+texture:SetAllPoints()
+texture:SetTexture("Interface/Buttons/UI-GuildButton-PublicNote-Up")
+texture:SetAlpha(0.5)
+local scroll = CreateFrame("ScrollFrame", "RUIChatHistoryScroll", UIParent, "UIPanelScrollFrameTemplate")
+scroll:SetSize(380,200)
+scroll:SetPoint("BOTTOMLEFT",UIParent,"BOTTOMLEFT",50,50)
+scroll:Hide()
+local texture2 = scroll:CreateTexture(nil, "BACKGROUND")
+texture2:SetSize(400,200)
+texture2:SetPoint("BOTTOMLEFT",UIParent,"BOTTOMLEFT",50,50)
+texture2:SetColorTexture(0.1,0.1,0.1,0.8)
+local editbox = CreateFrame("EditBox", nil, scroll)
+editbox:SetMultiLine(true)
+editbox:SetMaxLetters(99999)
+editbox:EnableMouse(true)
+editbox:SetAutoFocus(false)
+editbox:SetWidth(380)
+editbox:SetFont(ChatFrame1:GetFont(), 12)
+scroll:SetScrollChild(editbox)
+
+editbox:SetScript("OnEscapePressed", function() scroll:Hide() end)
+copy:SetScript("OnClick", function()
+	if scroll:IsShown() then
+		scroll:Hide()
+	else
+		editbox:SetText(CopyChat())
+		scroll:Show()
+	end
+end)
+
 local function ResizeChat1()
 	ChatFrame1:SetUserPlaced(true)
 	ChatFrame1:SetSize(C.db.chat.frame1Width, C.db.chat.frame1Height)
@@ -22,6 +70,8 @@ B:AddInitScript(function()
 		frame.buttonFrame:UnregisterAllEvents()
 		frame.buttonFrame.Show = frame.ScrollToBottomButton.Hide
 		frame:SetClampedToScreen(false)
+		local editbox =  _G["ChatFrame"..i.."EditBox"]
+		editbox:SetAltArrowKeyMode(false)
 	end
 	QuickJoinToastButton:Hide()
 	QuickJoinToastButton:UnregisterAllEvents()
@@ -41,4 +91,6 @@ B:AddInitScript(function()
 	ChatFrameChannelButton:ClearAllPoints()
 	ChatFrameChannelButton:SetPoint("BOTTOMLEFT", GeneralDockManager, "TOPLEFT")
 	ChatFrameChannelButton:SetAlpha(0.6)
+	--BNToastFrame:ClearAllPoints()
+	--B:SetupMover(BNToastFrame, "BNToastFrame",L["BNToastFrame"],true)
 end)
