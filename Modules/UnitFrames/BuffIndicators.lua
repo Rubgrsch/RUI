ad428fd37f6b47292cb176226bb2d0526db207ca
Rubgrsch <rubgrsch@gmail.com>
2020-11-26 00:03:29 +0800

    Fix buffIndicators

--- Modules/UnitFrames/BuffIndicators.lua
+++ Modules/UnitFrames/BuffIndicators.lua
@@ -5,8 +5,12 @@ local oUF = rui.oUF
 
 local function Update(self, _, unit)
 	if unit ~= self.unit then return end
+	for i=1, 4 do
+		local button = self.BuffIndicators[i]
+		button.shouldHide = true
+	end
 	for i=1, 40 do
-		local name, _, count, _, duration, expiration, _, _, _, spellId = UnitBuff(unit, i)
+		local name, icon, count, _, duration, expiration, _, _, _, spellId = UnitBuff(unit, i)
 		if name then
 			local p = C.buffIndicators[spellId]
 			if p then
@@ -14,11 +18,20 @@ local function Update(self, _, unit)
 				if duration and duration > 0 then
 					button.cd:SetCooldown(expiration - duration, duration)
 					button.cd:Show()
+					button.icon:Show()
+					button.shouldHide = false
 				else
-					button.cd:Hide()
+					button.shouldHide = true
 				end
 				button.count:SetText(count > 1 and count or "")
 			end
+		end
+	end
+	for i=1, 4 do
+		local button = self.BuffIndicators[i]
+		if button.shouldHide then
+			button.icon:Hide()
+			button.cd:Hide()
 		end
 	end
 end
@@ -31,6 +44,7 @@ local function Enable(self)
 			local button = self.BuffIndicators[v]
 			local _, _, fileID = GetSpellInfo(k)
 			button.icon:SetTexture(fileID)
+			button.icon:Hide()
 			flag = true
 		end
 	end
