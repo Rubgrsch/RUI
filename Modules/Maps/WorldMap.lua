033460539fa0ccd075a035120c21052989740bb9
Rubgrsch <rubgrsch@gmail.com>
2020-11-28 00:27:14 +0800

    Fix nil error

--- Modules/Maps/WorldMap.lua
+++ Modules/Maps/WorldMap.lua
@@ -33,10 +33,11 @@ local function UpdateMapID()
 	currentMapID = mapID
 	if not mapID then return end
 	local mapRect = mapRects[mapID]
-	if not mapRect then
-		mapRect = {}
-		mapRect[1] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0)))
-		mapRect[2] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1)))
+	if not mapRect then	
+		local p1 = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0)))
+		local p2 = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1)))
+		if not p1 or not p2 then return end
+		mapRect = {p1, p2}
 		mapRect[2]:Subtract(mapRect[1])
 		mapRects[mapID] = mapRect
 	end
