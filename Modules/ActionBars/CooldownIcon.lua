5f312cf7b6d8817a702f092529333db9b93bd9dd
Rubgrsch <rubgrsch@gmail.com>
2020-12-28 00:18:29 +0800

    Show a cooldown icon near cursor

---
+++ Modules/ActionBars/CooldownIcon.lua
@@ -0,0 +1,33 @@
+local _, rui = ...
+local B, L, C = unpack(rui)
+
+local cooldown = CreateFrame("Cooldown", nil, UIParent)
+cooldown:SetSize(20,20)
+cooldown:SetPoint("CENTER")
+cooldown:Hide()
+local icon = cooldown:CreateTexture(nil,"OVERLAY")
+icon:SetAllPoints()
+B:SetupCooldown(cooldown)
+cooldown:SetScript("OnUpdate",function(self,elapsed)
+	self.time = self.time + elapsed
+	if self.time > 1 then
+		cooldown:Hide()
+		self.time = 0
+	else
+		cooldown:SetAlpha(0.8*(1-self.time))
+	end
+end)
+
+cooldown:RegisterUnitEvent("UNIT_SPELLCAST_FAILED", "player")
+cooldown:SetScript("OnEvent", function(self,_,_,_,spellID)
+	local start, duration, enabled, modRate = GetSpellCooldown(spellID)
+	if duration < 2 then return end
+	cooldown:SetCooldown(start, duration, enabled, modRate)
+	local _,_, iconID = GetSpellInfo(spellID)
+	icon:SetTexture(iconID)
+	local x,y = GetCursorPosition()
+	local scale = self:GetEffectiveScale()
+	cooldown:SetPoint("TOPRIGHT", _G.UIParent, "BOTTOMLEFT",x/scale,y/scale)
+	cooldown.time = 0
+	cooldown:Show()
+end)
